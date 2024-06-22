6f3bfe95fdf6c659f5c8c33583e3c460ba1beb14
harishm-git <harishdeepakh@gmail.com>
2024-06-22 15:20:14 +0530

    fishshed project section sucessfully and now it is working

--- Portfolio/js.js
+++ Portfolio/js.js
@@ -43,3 +43,55 @@ services.forEach(element => {
   document.querySelector('.service-js').appendChild(divElem)
 
 });
+
+
+
+const Projects=[{
+  img:"project-photos/rps-game.png",
+  content:"This is an Rock Paper Scissor Game for JS Practice",
+  href:"https://harishm-git.github.io/rps-game/"
+},
+{
+  img:"project-photos/tdl.png",
+  content:"This is an To Do List for JS Practice",
+  href:"https://harishm-git.github.io/t-d-list/"
+},
+{
+  img:"project-photos/youtube.png",
+  href:"https://harishm-git.github.io/you-tube/" ,
+  content:"This is an Youtube Clone for Html and Css Practice"
+},
+{
+  img:"project-photos/amazon.png",
+  href:"https://harishm-git.github.io/amazon-project/" ,
+  content:"This is an Amazon Clone for Html and Css Practice"
+}]
+
+
+Projects.forEach((element)=>{
+
+  const anch=document.createElement('a');
+  anch.style="text-decoration:none";
+  anch.href=element.href;
+  const common_mo=document.createElement('div');
+  common_mo.className="common-model youtube-con";
+  const styleCo=document.createElement('div');
+  styleCo.className="style-container";
+  const Image=document.createElement('img');
+  Image.src=element.img;
+  Image.alt="photo about my project";
+  const text_pro=document.createElement('div');
+  text_pro.className="text-project";
+  const h_4=document.createElement('h4');
+  h_4.innerText=element.content;
+  styleCo.append(Image);
+  text_pro.append(h_4);
+  common_mo.append(styleCo);
+  common_mo.append(text_pro);
+  anch.append(common_mo);
+
+  document.querySelector('.feature-pro-container').appendChild(anch)
+
+
+
+})
