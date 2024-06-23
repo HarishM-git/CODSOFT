b60b948fe63a5820d09fc5f2ac38d4096f90f4c6
harishm-git <harishdeepakh@gmail.com>
2024-06-23 09:08:59 +0530

    added new scroll effects to my portfolio

--- Portfolio/js.js
+++ Portfolio/js.js
@@ -95,3 +95,77 @@ Projects.forEach((element)=>{
 
 
 })
+
+
+
+//scroll effects
+
+document.addEventListener('DOMContentLoaded', function() {
+  ScrollReveal().reveal('.header', { delay: 200, origin: 'top', distance: '100px' });
+  ScrollReveal().reveal('.home-text', { delay: 350, origin: 'bottom', distance: '100px' });
+  ScrollReveal().reveal('.intro-na',{
+    origin:'top',
+    distance:'100px',
+    duration:1000,
+    delay:350
+  })
+  ScrollReveal().reveal('.github-icon', {
+    origin: 'left',
+    distance: '200px',
+    duration: 1000,
+    delay: 200
+  });
+
+  ScrollReveal().reveal('.gmail-icon', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    delay: 400
+  });
+
+  ScrollReveal().reveal('.linkedin-icon', {
+    origin: 'top',
+    distance: '50px',
+    duration: 1000,
+    delay: 600
+  });
+
+  ScrollReveal().reveal('.facebook-icon', {
+    origin: 'right',
+    distance: '200px',
+    duration: 1000,
+    delay: 200
+  });
+  ScrollReveal().reveal('.service-text', { delay: 850, origin: 'right-bottom', distance: '100px' });
+  ScrollReveal().reveal('.web-developer', {
+    origin: 'left',
+    distance: '200px',
+    duration: 1000,
+    delay: 600
+  });
+
+
+
+
+  ScrollReveal().reveal('.feature-pro-text', { delay: 850, origin: 'bottom', distance: '100px' });
+  ScrollReveal().reveal('.common-model', {
+    origin: 'right-bottom',
+    distance: '200px',
+    duration: 1000,
+    delay: 600
+  });
+  ScrollReveal().reveal('.bio-text', { delay: 900, origin: 'left', distance: '200px' });
+  ScrollReveal().reveal('.contact-text', { delay: 850, origin: 'right-bottom', distance: '100px' });
+  ScrollReveal().reveal('.lft-in-ct', {
+    origin: 'right-bottom',
+    distance: '200px',
+    duration: 1000,
+    delay: 600
+  });
+  ScrollReveal().reveal('#hire', {
+    origin: 'left',
+    distance: '200px',
+    duration: 1000,
+    delay: 600
+  });
+});
